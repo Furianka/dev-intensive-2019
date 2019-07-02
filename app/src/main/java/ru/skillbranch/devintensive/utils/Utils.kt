cb918301eef59e491810cbc55866b11fb13721a7
Furianka <nataliya.yamont@gmail.com>
2019-07-02 16:56:46 +0500

    transliteration completed

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -8,8 +8,17 @@ object Utils {
         return Pair(if (firstName.isNullOrBlank()) null else firstName, if (lastName.isNullOrBlank()) null else lastName)
     }
 
-    fun transliteration(payload: String, divider: String = " "):String{
-        return ""
+    operator fun Regex.contains(text: CharSequence): Boolean = this.matches(text)
+
+    fun transliteration(payload: String, divider: String = " "):String {
+        val map = mapOf("а" to "a", "б" to "b", "в" to "v", "г" to "g", "д" to "d", "е" to "e", "ё" to "e", "ж" to "zh", "з" to "z", "и" to "i", "й" to "i", "к" to "k", "л" to "l", "м" to "m", "н" to "n", "о" to "o", "п" to "p", "р" to "r", "с" to "s", "т" to "t", "у" to "u", "ф" to "f", "х" to "h", "ц" to "c", "ч" to "ch", "ш" to "sh", "щ" to "sh'", "ъ" to "", "ы" to "i", "ь" to "", "э" to "e", "ю" to "yu", "я" to "ya", "А" to "A", "Б" to "B", "В" to "V", "Г" to "G", "Д" to "D", "Е" to "E", "Ё" to "E", "Ж" to "Zh", "З" to "Z", "И" to "I", "Й" to "I", "К" to "K", "Л" to "L", "М" to "M", "Н" to "N", "О" to "O", "П" to "P", "Р" to "R", "С" to "S", "Т" to "T", "У" to "U", "Ф" to "F", "Х" to "H", "Ц" to "C", "Ч" to "Ch", "Ш" to "Sh", "Щ" to "Sh'", "Ъ" to "", "Ы" to "I", "Ь" to "", "Э" to "E", "Ю" to "Yu", "Я" to "Ya")
+        return payload.replace(Regex("[${payload}]")){
+            when(it.value) {
+                in Regex("[\\w]")-> it.value
+                in Regex("[а-я,А-Я]")-> map.getValue(it.value)
+                else -> divider
+            }
+        }
     }
 
     fun toInitials(firstname: String?, lastname: String?):String{
