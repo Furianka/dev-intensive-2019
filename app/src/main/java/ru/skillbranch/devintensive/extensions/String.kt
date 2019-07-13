d5884d2fe22120f0db749b2c5c76453731df4538
Furianka <nataliya.yamont@gmail.com>
2019-07-13 19:34:36 +0500

    first 4 tasks completed

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -6,4 +6,4 @@ fun String.truncate(n : Int = 16): String?{
     else return temp.subSequence(0, n).trimEnd().toString() + "..."
 }
 
-fun String.stripHtml(): String? = this.replace("&amp;|&lt;|&gt;|&#39;|&quot;|&nbsp;".toRegex(),"").replace(Regex("<(/?[^>]+)>"),"").replace("[\\s]+".toRegex(), " ")
+fun String.stripHtml(): String? = this.replace("&amp;|&lt;|&gt;|&#39;|&quot;|&nbsp;".toRegex(),"").replace(Regex("</?[^>]+>"),"").replace("\\s+".toRegex(), " ")
